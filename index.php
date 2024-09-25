09bcc7d9447022740bb28d86887c7f93d74c6447
Snake Man <snakeman@phnet.fi>
2024-09-25 10:31:26 +0000

    my first commit, yippy

---
+++ index.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>Sea Power Naval Combat in the Missile Age</title>
+<LINK href="css.css" rel=stylesheet type="text/css">
+<META name="description" content="Sea Power Naval Combat in the Missile Age">
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+
+<?php include("/var/www/pmctactical.org/include/google-analytics.php"); ?>
+
+</head>
+<body>
+
+<header>
+<?php include("/var/www/include/ads.php"); ?>
+<?php include("/var/www/include/header-start.php"); ?>
+<h1>Sea Power Naval Combat in the Missile Age</h1>
+<p>Welcome to PMC website for naval warfare game: Sea Power Naval Combat in the Missile Age.</p>
+<?php include("/var/www/include/header-end.php"); ?>
+</header>
+
+<section>
+<?php include("/var/www/include/section-start.php"); ?>
+	<h2>Sea Power Game</h2>
+
+<p>
+Quote from microprose.com: <i>From the lead designer of Cold Waters, Sea Power lets you control NATO and Warsaw Pact forces in modern naval conflict campaigns. Use your advanced naval weaponry and sensors to respect rules of engagement and defeat the enemy forces in a tense fight for initiative and air/naval supremacy.</i>
+</p>
+
+<a href="https://www.triassicgames.com/" target="_blank">Triassic Games</a>, <a href="https://store.steampowered.com/app/1286220/Sea_Power__Naval_Combat_in_the_Missile_Age/" target="_blank">Sea Power in steam</a> and <a href="https://www.microprose.com/games/sea-power" target="_blank">Sea Power in microprose.com</a>.
+
+	<h2>Sea Power Video Links</h2>
+
+<p>
+Official <a href="https://www.youtube.com/watch?v=abqTIiyviH4" target="_blank">Sea Power Release Announcement Trailer - Microprose</a>.
+</p>
+
+<p>
+These youtube channels have some very high quality videos of Sea Power, some of them are even 4k high resolution so very enjoyable to watch. In no particular order.
+</p>
+
+<p>
+<a href="https://www.youtube.com/@Stealth17Gaming" target="_blank">Stealth17 Gaming</a>
+</p>
+
+<p>
+<a href="https://www.youtube.com/@Wolfpack345" target="_blank">Wolfpack345</a>
+</p>
+
+<p>
+<a href="https://www.youtube.com/@VulcanHDGaming" target="_blank">VulcanHDGaming</a>
+</p>
+
+<p>
+<a href="https://www.youtube.com/@Enigma89" target="_blank">Enigma89</a>
+</p>
+
+<p>
+<a href="" target="_blank"></a>
+</p>
+
+<?php include("/var/www/include/section-end.php"); ?>
+</section>
+
+<footer>
+<?php include("/var/www/include/footer-start.php"); ?>
+<p><a href="links.php">Links</a></p>
+<br><br>
+<p><i>PMC Sea Power 2024 - <?php print(date("Y")); ?>.</i></p>
+<?php include("/var/www/include/footer-end.php"); ?>
+<?php include("/var/www/include/support.php"); ?>
+<?php include("/var/www/include/w3-validator-logo.php"); ?>
+</footer>
+
+</body>
+</html>
---
+++ links.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>Links Sea Power Naval Combat in the Missile Age</title>
+<LINK href="css.css" rel=stylesheet type="text/css">
+<META name="description" content="Links Sea Power Naval Combat in the Missile Age">
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+
+<?php include("/var/www/pmctactical.org/include/google-analytics.php"); ?>
+
+</head>
+<body>
+
+<header>
+<?php include("/var/www/include/ads.php"); ?>
+<?php include("/var/www/include/header-start.php"); ?>
+<h1>Links Sea Power Naval Combat in the Missile Age</h1>
+<p>Useful web links Sea Power and PMC websites.</p>
+<?php include("/var/www/include/header-end.php"); ?>
+</header>
+
+<section>
+<?php include("/var/www/include/section-start.php"); ?>
+	<h2>PMC Website Links</h2>
+
+<?php include("/var/www/include/links.php"); ?>
+
+<?php include("/var/www/include/section-end.php"); ?>
+</section>
+
+<footer>
+<?php include("/var/www/include/footer-start.php"); ?>
+<p>Back to <a href="index.php">Sea Power root page</a></p>
+<br><br>
+<p><i>PMC Sea Power 2024 - <?php print(date("Y")); ?>.</i></p>
+<?php include("/var/www/include/footer-end.php"); ?>
+<?php include("/var/www/include/support.php"); ?>
+<?php include("/var/www/include/w3-validator-logo.php"); ?>
+</footer>
+
+</body>
+</html>
