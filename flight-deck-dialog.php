3443de24c21a66fa2ce1f8314e6e72e6fa95b884
Snake Man <snakeman@phnet.fi>
2024-10-11 06:14:25 +0000

    flight-deck-dialog page created

---
+++ flight-deck-dialog.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>Flight Deck Dialog Sea Power Naval Combat in the Missile Age</title>
+<LINK href="css.css" rel=stylesheet type="text/css">
+<META name="description" content="Flight Deck Dialog Sea Power Naval Combat in the Missile Age">
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+
+<?php include("/var/www/pmctactical.org/include/google-analytics.php"); ?>
+
+</head>
+<body>
+
+<header>
+<?php include("/var/www/include/ads.php"); ?>
+<?php include("/var/www/include/header-start.php"); ?>
+	<h1>Flight Deck Dialog Sea Power</h1>
+<p>Welcome to PMC website for naval warfare game: Sea Power Naval Combat in the Missile Age.</p>
+<?php include("/var/www/include/header-end.php"); ?>
+</header>
+
+<section>
+<?php include("/var/www/include/section-start.php"); ?>
+	<h2>Flight Deck Dialog Sea Power</h2>
+
+<img src="screenshots/Flight-Deck-Dialog-01.jpg" class="imgresponsive" loading="lazy" alt="Sea Power Flight Deck Dialog" title="Sea Power Flight Deck Dialog">
+<div class="imgcredit">
+Image credit: <a href="https://www.youtube.com/@Wolfpack345" target="_blank">Wolfpack345 youtube channel</a>, <a href="https://www.youtube.com/watch?v=fS25SReO9lc" target="_blank">SEA POWER First Look! || New Naval Simulation! video</a>.
+</div>
+
+<p>
+Flight Deck dialog is for launching and recovering aircrafts/helicopters from various platforms, aribases and ships.
+</p>
+
+<p>
+In the above example image we have aircraft capacity 1/1 meaning we only have one single aircraft. Type is SH-2F which is US Navy Sea Sprite helicopter. Loadout is Anti Submarine Warfare (ASW). Assigned drop down shows 1 as in this example there is only single aircraft available, if you'd have more then you could assign the number of airframes using that drop down. The big launch button simply launches whatever you've got selected. Flight operations box has the status of currently operating aircrafts/helicopters, are they taking off, landing, etc.
+</p>
+
+<?php include("/var/www/include/section-end.php"); ?>
+</section>
+
+<footer>
+<?php include("/var/www/include/footer-start.php"); ?>
+<p>
+<a href="links.php">Links</a>, <a href="index.php">Back to Home Page</a>.
+</p>
+<p><i>PMC Sea Power 2024 - <?php print(date("Y")); ?>.</i></p>
+<?php include("/var/www/include/footer-end.php"); ?>
+<?php include("/var/www/include/support.php"); ?>
+<?php include("/var/www/include/w3-validator-logo.php"); ?>
+</footer>
+
+</body>
+</html>
