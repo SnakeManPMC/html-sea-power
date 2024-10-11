8e4cc43bace5d71f8c38860caccbcc57d0b8e026
Snake Man <snakeman@phnet.fi>
2024-10-11 22:18:42 +0000

    weapon-reloading page created

---
+++ weapon-reloading.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>Weapon Reloading Sea Power Naval Combat in the Missile Age</title>
+<LINK href="css.css" rel=stylesheet type="text/css">
+<META name="description" content="Weapon Reloading Sea Power Naval Combat in the Missile Age">
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+
+<?php include("/var/www/pmctactical.org/include/google-analytics.php"); ?>
+
+</head>
+<body>
+
+<header>
+<?php include("/var/www/include/ads.php"); ?>
+<?php include("/var/www/include/header-start.php"); ?>
+	<h1>Weapon Reloading Sea Power</h1>
+<p>Welcome to PMC website for naval warfare game: Sea Power Naval Combat in the Missile Age.</p>
+<?php include("/var/www/include/header-end.php"); ?>
+</header>
+
+<section>
+<?php include("/var/www/include/section-start.php"); ?>
+	<h2>Weapon Reloading Sea Power</h2>
+
+<p>
+Units like ships can reload weapons, missiles for example. In the below example you see <a href="https://en.wikipedia.org/wiki/RIM-7_Sea_Sparrow" target="_blank">RIM-7 Sea Sparrow</a> surface to air missile being reloaded on board destroyer Paul F. Foster DD-964 which takes 26 minutes.
+</p>
+
+<img src="screenshots/Weapon-Reloading-01.jpg" class="imgresponsive" loading="lazy" alt="Sea Power Weapon Reloading" title="Sea Power Weapon Reloading">
+<div class="imgcredit">
+Image credit: <a href="https://www.youtube.com/@Wolfpack345" target="_blank">Wolfpack345 youtube channel</a>, <a href="https://www.youtube.com/watch?v=fS25SReO9lc" target="_blank">SEA POWER First Look! || New Naval Simulation! video</a>.
+</div>
+
+<p>
+Duration in the above example image is 26min 46sec, I assume "MK 29" is a magazine for the missile launcher or something like this. PMCTODO
+</p>
+
+<?php include("/var/www/include/section-end.php"); ?>
+</section>
+
+<footer>
+<?php include("/var/www/include/footer-start.php"); ?>
+<p>
+<a href="links.php">Links</a>, <a href="index.php">Back to Home Page</a>.
+</p>
+<p><i>PMC Sea Power 2024 - <?php print(date("Y")); ?>.</i></p>
+<?php include("/var/www/include/footer-end.php"); ?>
+<?php include("/var/www/include/support.php"); ?>
+<?php include("/var/www/include/w3-validator-logo.php"); ?>
+</footer>
+
+</body>
+</html>
