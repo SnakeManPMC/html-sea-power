ee6c09a0eb21014bab227f6b76f724618d4be6fb
Snake Man <snakeman@phnet.fi>
2024-10-12 10:36:49 +0000

    sonobuoy page created

---
+++ sonobuoy.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>Sonobuoy Sea Power Naval Combat in the Missile Age</title>
+<LINK href="css.css" rel=stylesheet type="text/css">
+<META name="description" content="Sonobuoy Sea Power Naval Combat in the Missile Age">
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+
+<?php include("/var/www/pmctactical.org/include/google-analytics.php"); ?>
+
+</head>
+<body>
+
+<header>
+<?php include("/var/www/include/ads.php"); ?>
+<?php include("/var/www/include/header-start.php"); ?>
+	<h1>Sonobuoy Sea Power</h1>
+<p>Welcome to PMC website for naval warfare game: Sea Power Naval Combat in the Missile Age.</p>
+<?php include("/var/www/include/header-end.php"); ?>
+</header>
+
+<section>
+<?php include("/var/www/include/section-start.php"); ?>
+	<h2>Sonobuoy Sea Power</h2>
+
+<p>
+<i>A sonobuoy (a portmanteau of sonar and buoy) is a small expendable sonar buoy dropped from aircraft or ships for anti-submarine warfare or underwater acoustic research. Sonobuoys are typically around 13 cm (5 in) in diameter and 91 cm (3 ft) long. When floating on the water, sonobuoys have both a radio transmitter above the surface and hydrophone sensors underwater.</i> -source <a href="https://en.wikipedia.org/wiki/Sonobuoy" target="_blank">Sonobuoy wikipedia</a>.
+</p>
+
+<p>
+Select a helicopter with ASW loadout, sonobuoys, then use CTRL-RMB click with sonobuoys to create a search pattern waypoints. See below example image.
+</p>
+
+<img src="screenshots/Sonobuoy-Helicopter-Pattern-01.jpg" class="imgresponsive" loading="lazy" alt="Sea Power Sonobuoy Helicopter Waypoints Pattern" title="Sea Power Sonobuoy Helicopter Waypoints Pattern">
+<div class="imgcredit">
+Image credit: <a href="https://www.youtube.com/@Wolfpack345" target="_blank">Wolfpack345 youtube channel</a>, <a href="https://www.youtube.com/watch?v=_RkaYshM_Jk" target="_blank">SEA POWER The Barney Incident || New Naval Simulation video</a>.
+</div>
+
+<p>
+In several Sea Power youtube videos I've seen players drop sonobuoys just to detect and track SURFACE ships as well if they don't want to use surface search radars, to me personally I never witnessed this in any other naval warfare sim previously, Harpoon, Janes Fleet Command, Dangerous Water, Cold Waters or CMANO, but hey maybe its just me. PMCTODO
+</p>
+
+<?php include("/var/www/include/section-end.php"); ?>
+</section>
+
+<footer>
+<?php include("/var/www/include/footer-start.php"); ?>
+<p>
+<a href="links.php">Links</a>, <a href="index.php">Back to Home Page</a>.
+</p>
+<p><i>PMC Sea Power 2024 - <?php print(date("Y")); ?>.</i></p>
+<?php include("/var/www/include/footer-end.php"); ?>
+<?php include("/var/www/include/support.php"); ?>
+<?php include("/var/www/include/w3-validator-logo.php"); ?>
+</footer>
+
+</body>
+</html>
