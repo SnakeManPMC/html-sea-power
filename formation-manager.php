fd38dd66aad94cb3c8c57ed9904923ecf4a50f89
Snake Man <snakeman@phnet.fi>
2024-10-12 21:11:29 +0000

    formation-manager page created

---
+++ formation-manager.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>Formation Manager Sea Power Naval Combat in the Missile Age</title>
+<LINK href="css.css" rel=stylesheet type="text/css">
+<META name="description" content="Formation Manager Sea Power Naval Combat in the Missile Age">
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+
+<?php include("/var/www/pmctactical.org/include/google-analytics.php"); ?>
+
+</head>
+<body>
+
+<header>
+<?php include("/var/www/include/ads.php"); ?>
+<?php include("/var/www/include/header-start.php"); ?>
+	<h1>Formation Manager Sea Power</h1>
+<p>Welcome to PMC website for naval warfare game: Sea Power Naval Combat in the Missile Age.</p>
+<?php include("/var/www/include/header-end.php"); ?>
+</header>
+
+<section>
+<?php include("/var/www/include/section-start.php"); ?>
+	<h2>Formation Manager Sea Power</h2>
+
+<p>
+Formation Manager is a index list of your units in a formation, this is not a <a href="unit-control-dialog.php">(unit control dialog) Formation Editor</a> tool.
+</p>
+
+<img src="screenshots/Formation-Manager-01.jpg" class="imgresponsive" loading="lazy" alt="Sea Power Formation Manager" title="Sea Power Formation Manager">
+<div class="imgcredit">
+Image credit: <a href="https://www.youtube.com/@Wolfpack345" target="_blank">Wolfpack345 youtube channel</a>, <a href="https://www.youtube.com/watch?v=zTuEJLN9hYU" target="_blank">SEA POWER Attempting to Recreate The Hunt For Red October || New Naval Simulation! video</a>.
+</div>
+
+<p>
+When you click a unit name in Formation Manager the 3D world view switches over to that unit.
+</p>
+
+<?php include("/var/www/include/section-end.php"); ?>
+</section>
+
+<footer>
+<?php include("/var/www/include/footer-start.php"); ?>
+<p>
+<a href="links.php">Links</a>, <a href="index.php">Back to Home Page</a>.
+</p>
+<p><i>PMC Sea Power 2024 - <?php print(date("Y")); ?>.</i></p>
+<?php include("/var/www/include/footer-end.php"); ?>
+<?php include("/var/www/include/support.php"); ?>
+<?php include("/var/www/include/w3-validator-logo.php"); ?>
+</footer>
+
+</body>
+</html>
