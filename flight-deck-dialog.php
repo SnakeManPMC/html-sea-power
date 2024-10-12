762a4ea54466097ea8ac84772e0f9997cee4fdcc
Snake Man <snakeman@phnet.fi>
2024-10-12 09:31:41 +0000

    flight-deck-dialog screenshot added

--- flight-deck-dialog.php
+++ flight-deck-dialog.php
@@ -37,6 +37,15 @@ Flight Deck dialog is for launching and recovering aircrafts/helicopters from va
 In the above example image we have aircraft capacity 1/1 meaning we only have one single aircraft. Type is SH-2F which is US Navy Sea Sprite helicopter. Loadout is Anti Submarine Warfare (ASW). Assigned drop down shows 1 as in this example there is only single aircraft available, if you'd have more then you could assign the number of airframes using that drop down. The big launch button simply launches whatever you've got selected. Flight operations box has the status of currently operating aircrafts/helicopters, are they taking off, landing, etc.
 </p>
 
+<p>
+Below example image is flight deck dialog from USS Nimitz recovering many aircrafts.
+</p>
+
+<img src="screenshots/Flight-Deck-Dialog-02.jpg" class="imgresponsive" loading="lazy" alt="Sea Power Flight Deck Dialog USS Nimitz Recovering Aircrafts" title="Sea Power Flight Deck Dialog USS Nimitz Recovering Aircrafts">
+<div class="imgcredit">
+Image credit: <a href="https://www.youtube.com/@Wolfpack345" target="_blank">Wolfpack345 youtube channel</a>, <a href="https://www.youtube.com/watch?v=Ob5npjC-beo" target="_blank">SEA POWER Dance of the Vampires || New Naval Simulation video</a>.
+</div>
+
 <?php include("/var/www/include/section-end.php"); ?>
 </section>
 
